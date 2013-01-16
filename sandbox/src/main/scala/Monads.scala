58f7cf3d3b8ead9edd8244f6163363e4b6113640
Dan Rosen <dan@marakana.com>
2013-01-16 14:05:44 -0800

    implemented Option

--- sandbox/src/main/scala/Monads.scala
+++ sandbox/src/main/scala/Monads.scala
@@ -1,15 +1,29 @@
 object Monads {
 
+  /*
+   * Monads represent sequencing/composing operations through
+   * some computational context. The monad encapsulates the context
+   * and allows the programmer to focus on the sequence of operations.
+   */
+
+  // Option represents the context of possible failures
   sealed trait Option[+A] {
     def map[B](fn: A => B): Option[B]
+    def flatMap[B](fn: A => Option[B]): Option[B]
   }
+
   object Option {
+
     case class Some[+A](value: A) extends Option[A] {
-      def map[B](fn: A => B): Option[B] = ???
+      def map[B](fn: A => B): Option[B] = Some(fn(value))
+      def flatMap[B](fn: A => Option[B]): Option[B] = fn(value)
     }
+
     case object None extends Option[Nothing] {
-      def map[B](fn: Nothing => B): Option[B] = ???
+      def map[B](fn: Nothing => B): Option[B] = None
+      def flatMap[B](fn: Nothing => Option[B]): Option[B] = None
     }
+
   }
 
 }
