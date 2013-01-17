6dafac220b6b0ee65108f62a3d0e569f57f8c5bc
Dan Rosen <dan@marakana.com>
2013-01-16 18:09:12 -0800

    more typeclass-fu, and context bounds

--- sandbox/src/main/scala/Typeclasses.scala
+++ sandbox/src/main/scala/Typeclasses.scala
@@ -12,13 +12,32 @@ object Typeclasses {
     def map[A, B](fa: Option[A], fn: A => B): Option[B] = fa.map(fn)
   }
 
-  def addOne[F[_]](f: F[Int])(implicit functor: Functor[F]): F[Int] =
-    functor.map(f, {a: Int => a + 1})
+  def addOne[F[_] : Functor](f: F[Int]): F[Int] =
+    implicitly[Functor[F]].map(f, {a: Int => a + 1})
 
   trait Monoid[A] {
     def zero: A
     def plus(lhs: A, rhs: A): A
   }
 
-  val intAdditionMonoid: Monoid[Int] = ???
+  implicit val intAdditionMonoid: Monoid[Int] = new Monoid[Int] {
+    def zero: Int = 0
+    def plus(lhs: Int, rhs: Int): Int = lhs + rhs
+  }
+
+  implicit val stringMonoid: Monoid[String] = new Monoid[String] {
+    def zero: String = ""
+    def plus(lhs: String, rhs: String): String = lhs + rhs
+  }
+
+  implicit def functionMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
+    def zero: A => A = identity
+    def plus(lhs: A => A, rhs: A => A): A => A = lhs andThen rhs
+  }
+
+  def sum[A : Monoid](l: List[A]): A = {
+    val monoid = implicitly[Monoid[A]]
+    l.foldLeft(monoid.zero)(monoid.plus)
+  }
+
 }
